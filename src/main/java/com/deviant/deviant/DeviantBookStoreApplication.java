a4b37f927f42b7c9dd7e4896998023b9ec6849a4
Temirlan Emilbekov <emilbekov.tute@gmail.com>
2024-10-08 10:48:32 +0600

    Initial commit

---
+++ src/main/java/com/deviant/deviant/DeviantBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.deviant.deviant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviantBookStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeviantBookStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/deviant/deviant/DeviantBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deviant.deviant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviantBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
